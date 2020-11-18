6e21b6631e362c1071fb682be340e76aa0e91cba
Pedro Andrade <realpeh@gmail.com>
2020-11-18 20:11:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   const origin =
     process.env.NODE_ENV !== "production"
       ? "http://localhost:3000"
-      : "https://whomakeesports.com";
+      : "https://whomakesesports.com";
 
   const res = await fetch(`${origin}/api/designers`);
   const designers = await res.json();
